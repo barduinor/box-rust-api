1d4866756ee679ac449ce6d4dddaf6bad11d40d3
barduinor <barduinor@gmail.com>
2023-06-02 12:55:03 -0400

    authorization listening to http request

--- src/authorization_main.rs
+++ src/authorization_main.rs
@@ -1,12 +1,32 @@
 // use cargo run --bin authorization_main to run this file
 use std::env;
+use std::process::exit;
 
 // use dotenv;
 // use webbrowser;
 
+use tiny_http::Request;
+use tiny_http::Response;
+use url::Url;
+
 use openapi::apis::authorization_api::get_authorizarion_url;
 use openapi::apis::authorization_api::GetAuthorizeParams;
+
 // use openapi::apis::configuration::Configuration;
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct UrlParams {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    code: Option<String>,
+
+    state: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    error_description: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -15,6 +35,9 @@ async fn main() {
     // let developer_token = env::var("DEVELOPER_TOKEN").expect("DEVELOPER_TOKEN not set");
     let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
     let redirect_uri = env::var("REDIRECT_URI").expect("REDIRECT_URI not set");
+
+    let hostname = env::var("HOSTNAME").expect("HOSTNAME not set");
+    let port = env::var("PORT").expect("PORT not set");
 
     // let config = Configuration {
     //     oauth_access_token: Some(developer_token),
@@ -41,5 +64,56 @@ async fn main() {
         println!("Opened browser successfully");
     } else {
         println!("Failed to open browser");
+    }
+    let hostname_port = hostname + ":" + &port;
+    let server = tiny_http::Server::http(hostname_port).unwrap();
+    println!("Listening on {}", server.server_addr());
+
+    match server.recv() {
+        Ok(rq) => {
+            println!(
+                "received request:\nmethod: {:?}\nurl: {:?}\nremote_addr: {:?} ",
+                rq.method(),
+                rq.url(),
+                rq.remote_addr(),
+                // rq.headers()
+            );
+
+            let base_url = match Url::parse("http://127.0.0.1") {
+                Ok(url) => url,
+                Err(e) => {
+                    println!("error: {}", e);
+                    exit(1)
+                }
+            };
+
+            let url = match base_url.join(rq.url()) {
+                Ok(url) => url,
+                Err(e) => {
+                    println!("error: {}", e);
+                    exit(1)
+                }
+            };
+
+            let query_params: UrlParams = match serde_qs::from_str(url.query().unwrap()) {
+                Ok(query_params) => query_params,
+                Err(e) => {
+                    println!("error: {}", e);
+                    exit(1)
+                }
+            };
+
+            if query_params.error.is_some() {
+                println!("error: {}", query_params.error_description.unwrap());
+                exit(1)
+            }
+
+            let response = Response::empty(200);
+            rq.respond(response);
+        }
+        Err(e) => {
+            println!("error: {}", e);
+            exit(1)
+        }
     }
 }
