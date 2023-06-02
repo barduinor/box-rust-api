7a95ac08f4221a4ccc003fd6b3ce1989a22c9d4e
barduinor <barduinor@gmail.com>
2023-06-02 09:57:33 -0400

    authorization

--- src/authorization_main.rs
+++ src/authorization_main.rs
@@ -1,35 +1,37 @@
 // use cargo run --bin authorization_main to run this file
 use std::env;
 
-use dotenv;
-use webbrowser;
+// use dotenv;
+// use webbrowser;
 
-use openapi::apis::configuration::Configuration;
-use openapi::apis::authorization_api::GetAuthorizeParams;
 use openapi::apis::authorization_api::get_authorizarion_url;
-
-
+use openapi::apis::authorization_api::GetAuthorizeParams;
+// use openapi::apis::configuration::Configuration;
 
 #[tokio::main]
-async fn main(){
+async fn main() {
     dotenv::dotenv().expect("Failed to read .env file");
 
-    let developer_token = env::var("DEVELOPER_TOKEN").expect("DEVELOPER_TOKEN not set");
+    // let developer_token = env::var("DEVELOPER_TOKEN").expect("DEVELOPER_TOKEN not set");
     let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
     let redirect_uri = env::var("REDIRECT_URI").expect("REDIRECT_URI not set");
 
-    let mut config = Configuration::default();
-    // TODO: Bearer token is being ignored, consider fixing
-    // config.bearer_access_token = Some(developer_token);
-    config.oauth_access_token = Some(developer_token);
+    // let config = Configuration {
+    //     oauth_access_token: Some(developer_token),
+    //     // TODO: Bearer token is being ignored, consider fixing
+    //     // config.bearer_access_token = Some(developer_token);
+    //     ..Default::default()
+    // };
     // println!("{:?}", config);
 
-    let mut params = GetAuthorizeParams::default();
-    params.client_id = client_id;
-    params.redirect_uri = Some(redirect_uri);
-    // params.response_type = "code".to_string();
-    params.state = Some("STATE_STATE".to_string());
-    params.scope = Some("root_readwrite".to_string());
+    let params = GetAuthorizeParams {
+        client_id,
+        redirect_uri: Some(redirect_uri),
+        // state: Some("STATE_STATE".to_string()),
+        // scope: Some("root_readwrite".to_string()),
+        ..Default::default()
+    };
+
     println!("\nParams: {:?}\n", params);
 
     let authorization_url = get_authorizarion_url(params);
@@ -40,7 +42,4 @@ async fn main(){
     } else {
         println!("Failed to open browser");
     }
-
-    
-
 }
